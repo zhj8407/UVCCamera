62174d8d8a3500336aba9a035957e597e0f202d8
Jerry Zhang <Jerry.Zhang@polycom.com>
2017-09-15 23:59:01 +0800

    Add more recording params in UVCCameraHandler

--- usbCameraCommon/src/main/java/com/serenegiant/usbcameracommon/UVCCameraHandler.java
+++ usbCameraCommon/src/main/java/com/serenegiant/usbcameracommon/UVCCameraHandler.java
@@ -30,106 +30,107 @@ import com.serenegiant.widget.CameraViewInterface;
 
 public class UVCCameraHandler extends AbstractUVCCameraHandler {
 
-	/**
-	 * create UVCCameraHandler, use MediaVideoEncoder, try MJPEG, default bandwidth
-	 * @param parent
-	 * @param cameraView
-	 * @param width
-	 * @param height
-	 * @return
-	 */
-	public static final UVCCameraHandler createHandler(
-			final Activity parent, final CameraViewInterface cameraView,
-			final int width, final int height) {
+    /**
+     * create UVCCameraHandler, use MediaVideoEncoder, try MJPEG, default bandwidth
+     */
+    public static final UVCCameraHandler createHandler(
+            final Activity parent, final CameraViewInterface cameraView,
+            final int width, final int height) {
 
-		return createHandler(parent, cameraView, 1, width, height, UVCCamera.FRAME_FORMAT_MJPEG, UVCCamera.DEFAULT_BANDWIDTH);
-	}
+        return createHandler(parent, cameraView, 1, width, height, UVCCamera.FRAME_FORMAT_MJPEG,
+                UVCCamera.DEFAULT_BANDWIDTH,
+                UVCCamera.DEFAULT_RECORD_WIDTH, UVCCamera.DEFAULT_RECORD_HEIGHT,
+                UVCCamera.DEFAULT_RECORD_MODE, UVCCamera.DEFAULT_RECORD_PROFILE,
+                UVCCamera.DEFAULT_BANDWIDTH);
+    }
 
-	/**
-	 * create UVCCameraHandler, use MediaVideoEncoder, try MJPEG
-	 * @param parent
-	 * @param cameraView
-	 * @param width
-	 * @param height
-	 * @param bandwidthFactor
-	 * @return
-	 */
-	public static final UVCCameraHandler createHandler(
-			final Activity parent, final CameraViewInterface cameraView,
-			final int width, final int height, final float bandwidthFactor) {
+    /**
+     * create UVCCameraHandler, use MediaVideoEncoder, try MJPEG
+     */
+    public static final UVCCameraHandler createHandler(
+            final Activity parent, final CameraViewInterface cameraView,
+            final int width, final int height, final float bandwidthFactor) {
 
-		return createHandler(parent, cameraView, 1, width, height, UVCCamera.FRAME_FORMAT_MJPEG, bandwidthFactor);
-	}
+        return createHandler(parent, cameraView, 1, width, height, UVCCamera.FRAME_FORMAT_MJPEG,
+                bandwidthFactor,
+                UVCCamera.DEFAULT_RECORD_WIDTH, UVCCamera.DEFAULT_RECORD_HEIGHT,
+                UVCCamera.DEFAULT_RECORD_MODE, UVCCamera.DEFAULT_RECORD_PROFILE,
+                UVCCamera.DEFAULT_BANDWIDTH);
+    }
 
-	/**
-	 * create UVCCameraHandler, try MJPEG, default bandwidth
-	 * @param parent
-	 * @param cameraView
-	 * @param encoderType 0: use MediaSurfaceEncoder, 1: use MediaVideoEncoder, 2: use MediaVideoBufferEncoder
-	 * @param width
-	 * @param height
-	 * @return
-	 */
-	public static final UVCCameraHandler createHandler(
-			final Activity parent, final CameraViewInterface cameraView,
-			final int encoderType, final int width, final int height) {
+    /**
+     * create UVCCameraHandler, try MJPEG, default bandwidth
+     *
+     * @param encoderType 0: use MediaSurfaceEncoder, 1: use MediaVideoEncoder, 2: use
+     *                    MediaVideoBufferEncoder
+     */
+    public static final UVCCameraHandler createHandler(
+            final Activity parent, final CameraViewInterface cameraView,
+            final int encoderType, final int width, final int height) {
 
-		return createHandler(parent, cameraView, encoderType, width, height, UVCCamera.FRAME_FORMAT_MJPEG, UVCCamera.DEFAULT_BANDWIDTH);
-	}
+        return createHandler(parent, cameraView, encoderType, width, height,
+                UVCCamera.FRAME_FORMAT_MJPEG, UVCCamera.DEFAULT_BANDWIDTH,
+                UVCCamera.DEFAULT_RECORD_WIDTH, UVCCamera.DEFAULT_RECORD_HEIGHT,
+                UVCCamera.DEFAULT_RECORD_MODE, UVCCamera.DEFAULT_RECORD_PROFILE,
+                UVCCamera.DEFAULT_BANDWIDTH);
+    }
 
-	/**
-	 * create UVCCameraHandler, default bandwidth
-	 * @param parent
-	 * @param cameraView
-	 * @param encoderType 0: use MediaSurfaceEncoder, 1: use MediaVideoEncoder, 2: use MediaVideoBufferEncoder
-	 * @param width
-	 * @param height
-	 * @param format either UVCCamera.FRAME_FORMAT_YUYV(0) or UVCCamera.FRAME_FORMAT_MJPEG(1)
-	 * @return
-	 */
-	public static final UVCCameraHandler createHandler(
-			final Activity parent, final CameraViewInterface cameraView,
-			final int encoderType, final int width, final int height, final int format) {
+    /**
+     * create UVCCameraHandler, default bandwidth
+     *
+     * @param encoderType 0: use MediaSurfaceEncoder, 1: use MediaVideoEncoder, 2: use
+     *                    MediaVideoBufferEncoder
+     * @param format      either UVCCamera.FRAME_FORMAT_YUYV(0) or UVCCamera.FRAME_FORMAT_MJPEG(1)
+     */
+    public static final UVCCameraHandler createHandler(
+            final Activity parent, final CameraViewInterface cameraView,
+            final int encoderType, final int width, final int height, final int format) {
 
-		return createHandler(parent, cameraView, encoderType, width, height, format, UVCCamera.DEFAULT_BANDWIDTH);
-	}
+        return createHandler(parent, cameraView, encoderType, width, height, format,
+                UVCCamera.DEFAULT_BANDWIDTH,
+                UVCCamera.DEFAULT_RECORD_WIDTH, UVCCamera.DEFAULT_RECORD_HEIGHT,
+                UVCCamera.DEFAULT_RECORD_MODE, UVCCamera.DEFAULT_RECORD_PROFILE,
+                UVCCamera.DEFAULT_BANDWIDTH);
+    }
 
-	/**
-	 * create UVCCameraHandler
-	 * @param parent
-	 * @param cameraView
-	 * @param encoderType 0: use MediaSurfaceEncoder, 1: use MediaVideoEncoder, 2: use MediaVideoBufferEncoder
-	 * @param width
-	 * @param height
-	 * @param format either UVCCamera.FRAME_FORMAT_YUYV(0) or UVCCamera.FRAME_FORMAT_MJPEG(1)
-	 * @param bandwidthFactor
-	 * @return
-	 */
-	public static final UVCCameraHandler createHandler(
-			final Activity parent, final CameraViewInterface cameraView,
-			final int encoderType, final int width, final int height, final int format, final float bandwidthFactor) {
+    /**
+     * create UVCCameraHandler
+     *
+     * @param encoderType   0: use MediaSurfaceEncoder, 1: use MediaVideoEncoder, 2: use
+     *                      MediaVideoBufferEncoder
+     * @param previewFormat either UVCCamera.FRAME_FORMAT_YUYV(0) or UVCCamera.FRAME_FORMAT_MJPEG(1)
+     */
+    public static final UVCCameraHandler createHandler(
+            final Activity parent, final CameraViewInterface cameraView,
+            final int encoderType, final int previewWidth, final int previewHeight,
+            final int previewFormat,
+            final float previewBandwidthFactor,
+            int recordWidth, int recordHeight, int recordFormat, int recordProfile,
+            float recordBandwidthFactor) {
 
-		final CameraThread thread = new CameraThread(UVCCameraHandler.class, parent, cameraView, encoderType, width, height, format, bandwidthFactor);
-		thread.start();
-		return (UVCCameraHandler)thread.getHandler();
-	}
+        final CameraThread thread = new CameraThread(UVCCameraHandler.class, parent, cameraView,
+                encoderType, previewWidth, previewHeight, previewFormat, previewBandwidthFactor,
+                recordWidth, recordHeight, recordFormat, recordProfile, recordBandwidthFactor);
+        thread.start();
+        return (UVCCameraHandler) thread.getHandler();
+    }
 
-	protected UVCCameraHandler(final CameraThread thread) {
-		super(thread);
-	}
+    protected UVCCameraHandler(final CameraThread thread) {
+        super(thread);
+    }
 
-	@Override
-	public void startPreview(final Object surface) {
-		super.startPreview(surface);
-	}
+    @Override
+    public void startPreview(final Object surface) {
+        super.startPreview(surface);
+    }
 
-	@Override
-	public void captureStill() {
-		super.captureStill();
-	}
+    @Override
+    public void captureStill() {
+        super.captureStill();
+    }
 
-	@Override
-	public void captureStill(final String path) {
-		super.captureStill(path);
-	}
+    @Override
+    public void captureStill(final String path) {
+        super.captureStill(path);
+    }
 }
